8862f3af4fa1b95b3146d9b5f02ded71eed3c493
Sandhya Dasu <sadasu@cisco.com>
2015-10-15 11:10:33 -0400

    Support for multiple UCS Managers
    Cisco ML2 UCS Manager plugin us being extended to support multiple UCS
    Manager instances within a single Openstack cloud.
    
    Change-Id: I890470a17b522b6e39a2578937c0537bcf4c09cf
    Closes-Bug: #1491025

--- networking_cisco/plugins/ml2/drivers/cisco/ucsm/config.py
+++ networking_cisco/plugins/ml2/drivers/cisco/ucsm/config.py
@@ -14,8 +14,11 @@
 #    under the License.
 
 from oslo_config import cfg
+from oslo_log import log as logging
 
 from networking_cisco.plugins.ml2.drivers.cisco.ucsm import constants as const
+
+LOG = logging.getLogger(__name__)
 
 """ Cisco UCS Manager ML2 Mechanism driver specific configuration.
 
@@ -26,26 +29,13 @@ create UCS Manager port profiles.
 """
 
 ml2_cisco_ucsm_opts = [
-    cfg.StrOpt('ucsm_ip',
-               help=_('Cisco UCS Manager IP address. This is a required field '
-                      'to communicate with a Cisco UCS Manager.')),
-    cfg.StrOpt('ucsm_username',
-               help=_('Username for UCS Manager. This is a required field '
-                      'to communicate with a Cisco UCS Manager.')),
-    cfg.StrOpt('ucsm_password',
-               secret=True,  # do not expose value in the logs
-               help=_('Password for UCS Manager. This is a required field '
-                      'to communicate with a Cisco UCS Manager.')),
     cfg.ListOpt('supported_pci_devs',
                 default=[const.PCI_INFO_CISCO_VIC_1240,
                          const.PCI_INFO_INTEL_82599],
                 help=_('List of comma separated vendor_id:product_id of '
                        'SR_IOV capable devices supported by this MD. This MD '
                        'supports both VM-FEX and SR-IOV devices.')),
-    cfg.ListOpt('ucsm_host_list',
-                help=_('List of comma separated Host:Service Profile tuples '
-                       'providing the Service Profile associated with each '
-                       'Host to be supported by this MD.')),
+
 ]
 
 cfg.CONF.register_opts(ml2_cisco_ucsm_opts, "ml2_cisco_ucsm")
@@ -63,14 +53,33 @@ def parse_pci_vendor_config():
     return vendor_list
 
 
-def parse_ucsm_host_config():
-    host_dict = {}
-    host_config_list = cfg.CONF.ml2_cisco_ucsm.ucsm_host_list
-    for host in host_config_list:
-        host_sp = host.split(':')
-        if len(host_sp) != 2:
-            raise cfg.Error(_("UCS Mech Driver: Invalid Host Service "
-                              "Profile config: %s") % host)
-        key = host_sp[0]
-        host_dict[key] = host_sp[1]
-    return host_dict
+class UcsmConfig(object):
+    """ML2 Cisco UCSM Mechanism Driver Configuration class."""
+    ucsm_dict = {}
+
+    def __init__(self):
+        self._create_ucsm_dict()
+
+    def _create_ucsm_dict(self):
+        """Create a dictionary of all UCS Manager data from the config file."""
+        multi_parser = cfg.MultiConfigParser()
+        read_ok = multi_parser.read(cfg.CONF.config_file)
+
+        if len(read_ok) != len(cfg.CONF.config_file):
+            raise cfg.Error(_('Some config files were not parsed properly'))
+
+        for parsed_file in multi_parser.parsed:
+            for parsed_item in parsed_file.keys():
+                dev_id, sep, dev_ip = parsed_item.partition(':')
+                if dev_id.lower() == 'ml2_cisco_ucsm_ip':
+                    ucsm_info = []
+                    for dev_key, value in parsed_file[parsed_item].items():
+                        ucsm_info.append(value[0])
+                    self.ucsm_dict[dev_ip] = ucsm_info
+
+    def get_credentials_for_ucsm_ip(self, ucsm_ip):
+        if ucsm_ip in self.ucsm_dict:
+            return self.ucsm_dict[ucsm_ip]
+
+    def get_all_ucsm_ips(self):
+        return self.ucsm_dict.keys()
