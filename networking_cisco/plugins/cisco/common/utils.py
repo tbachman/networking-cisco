4272b06a58ef4214378213ac573bff6c4b9694ba
Bob Melander <bob.melander@gmail.com>
2015-11-09 15:37:34 +0100

    Move and rename mocking of ncclient for end-2-end tests w/o devices
    This patch moves the mocking function to utils.py. Whenever mocking
    is needed that module need only be imported and the mock_ncclient()
    be called.
    
    Change-Id: I569fc4ffcb1015577b57151afc73fcc7cfdc08af

--- networking_cisco/plugins/cisco/common/utils.py
+++ networking_cisco/plugins/cisco/common/utils.py
@@ -77,3 +77,51 @@ def convert_validate_driver_class(driver_class_name):
             LOG.error(_LE('Failed to verify driver module %(name)s: %(err)s'),
                       {'name': driver_class_name, 'err': e})
     raise DriverNotFound(driver=driver_class_name)
+
+
+# NOTE(bobmel): call _mock_ncclient() in main() of cfg_agent.py to run config
+# agent with fake ncclient. That mocked mode of running the config agent is
+# useful for end-2-end-like debugging without actual backend hosting devices.
+def mock_ncclient():
+    import mock
+
+    targets = ['networking_cisco.plugins.cisco.cfg_agent.device_drivers.'
+               'csr1kv.csr1kv_routing_driver.manager',
+               'networking_cisco.plugins.cisco.cfg_agent.device_drivers.'
+               'csr1kv.iosxe_routing_driver.manager']
+    ncc_patchers = []
+    ncclient_mock = mock.MagicMock()
+    ok_xml_obj = mock.MagicMock()
+    ok_xml_obj.xml = "<ok />"
+    ncclient_mock.connect.return_value.edit_config.return_value = ok_xml_obj
+    for target in targets:
+        patcher = mock.patch(target, ncclient_mock)
+        patcher.start()
+        ncc_patchers.append(patcher)
+
+    targets = ['networking_cisco.plugins.cisco.cfg_agent.device_drivers'
+               '.csr1kv.csr1kv_routing_driver.CSR1kvRoutingDriver.'
+               '_get_running_config',
+               'networking_cisco.plugins.cisco.cfg_agent.device_drivers.'
+               'csr1kv.iosxe_routing_driver.IosXeRoutingDriver.'
+               '_get_running_config',
+               'networking_cisco.plugins.cisco.cfg_agent.device_drivers.'
+               'asr1k.asr1k_cfg_syncer.ConfigSyncer.get_running_config']
+    fake_running_config = ("interface GigabitEthernet1\n"
+                           "ip address 10.0.0.10 255.255.255.255\n"
+                           "ip route 0.0.0.0 0.0.0.0 GigabitEthernet1 "
+                           "10.0.0.1")
+    g_r_c_patchers = []
+    g_r_c_mock = mock.MagicMock(return_value=fake_running_config)
+    empty_g_r_c_mock = mock.MagicMock(return_value=[""])
+    g_r_c_mocks = [g_r_c_mock, g_r_c_mock, empty_g_r_c_mock]
+    for i in range(len(targets)):
+        patcher = mock.patch(targets[i], g_r_c_mocks[i])
+        patcher.start()
+        g_r_c_patchers.append(patcher)
+
+    is_pingable_mock = mock.MagicMock(return_value=True)
+    pingable_patcher = mock.patch(
+        'networking_cisco.plugins.cisco.cfg_agent.device_status._is_pingable',
+        is_pingable_mock)
+    pingable_patcher.start()
